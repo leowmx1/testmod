a27ed1143cbd3b66fcb097d8c25d6f0b8d49bbdf
王铭瑄 <leowmx@163.com>
2026-01-05 17:47:51 +0800

    Fix mixin bugs

--- src/client/java/io/github/leowmx/testmod/mixin/client/ExampleClientMixin.java
+++ src/client/java/io/github/leowmx/testmod/mixin/client/ExampleClientMixin.java
@@ -1,6 +1,8 @@
 package io.github.leowmx.testmod.mixin.client;
 
 import net.minecraft.client.MinecraftClient;
+import net.minecraft.client.world.ClientWorld;
+
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
@@ -8,8 +10,8 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(MinecraftClient.class)
 public class ExampleClientMixin {
-	@Inject(at = @At("HEAD"), method = "run")
-	private void init(CallbackInfo info) {
-		// This code is injected into the start of MinecraftClient.run()V
-	}
+	@Inject(method = "setWorld", at = @At("TAIL"))
+    private void onWorldSet(ClientWorld world, CallbackInfo ci) {
+        // 客户端世界已切换
+    }
 }
